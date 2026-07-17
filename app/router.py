43e2017a55bdfbeed851b89201f149abfbb7fa0c
sir20409 <yisamgye.s240183@ggh.goe.go.kr>
2026-07-17 15:59:50 +0900

    Update router.py

--- app/router.py
+++ app/router.py
@@ -3,7 +3,11 @@ from app.service import get_analysis_result
 
 router = APIRouter()
 
-@router.get("/api/analysis")
+@router.get(
+    "/api/analysis",
+    summary="범죄 데이터 회귀분석 결과 조회",
+    description="Y1(보이스피싱), Y2(인터넷 사기)에 대한 회귀분석 결과 반환"
+)
 def analysis():
     """
     분석 결과 반환
