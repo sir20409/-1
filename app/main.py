8ee441f959b577818120e3282d21bf3700bcec98
sir20409 <yisamgye.s240183@ggh.goe.go.kr>
2026-07-17 17:29:36 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,29 +1,51 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 from app.router import router
 from app.service import initialize_analysis
 
 
 app = FastAPI(
-    title="범죄 데이터 분석 시스템",
-    description="보이스피싱 및 인터넷 사기 회귀분석 API",
-    version="1.0.0"
+    title="범죄 데이터 분석 시스템"
 )
 
 
-# API 연결
+app.mount(
+    "/images",
+    StaticFiles(directory="images"),
+    name="images"
+)
+
+
 app.include_router(router)
 
 
-# 서버 시작 시 분석 실행
+
 @app.on_event("startup")
 def startup_event():
     initialize_analysis()
 
 
-@app.get("/")
-def root():
-    return {
-        "message": "Server Running",
-        "docs": "/docs"
-    }
+
+@app.get("/", response_class=HTMLResponse)
+def home():
+
+    return """
+    <html>
+    <body>
+
+    <h1>범죄 데이터 분석 결과</h1>
+
+    <h2>Y1 보이스피싱</h2>
+    <img src="/images/Y1_prediction.png" width="500">
+    <img src="/images/Y1_residual.png" width="500">
+
+
+    <h2>Y2 인터넷 사기</h2>
+    <img src="/images/Y2_prediction.png" width="500">
+    <img src="/images/Y2_residual.png" width="500">
+
+    </body>
+    </html>
+    """
